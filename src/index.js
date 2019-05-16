46213990db93c8f214423a2d334984b18d71935c
JairoCaliz <epistemologiaunicor@gmail.com>
2019-05-16 20:14:45 +0000

    cambio de la ruta origen

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola mundo");
+  res.send("Bienvenido a tu portafolio creativo");
 });
 
 app.get("/users", (req, res) => {
